7205dc29c383b6020125ecc1ea4cddfddaa2a66e
Cori1304 <160833798+Cori1304-Seong@users.noreply.github.com>
2025-02-28 14:16:36 +0900

    Time: 22 ms (93.16%), Space: 56.6 MB (91.31%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,9 +1,10 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-       int[] answer = new int[temperatures.length];
-        Stack<Integer> stack = new Stack<>();
+      int length = temperatures.length;
+        int[] answer = new int[length];
+        Deque<Integer> stack = new ArrayDeque<>();
 
-        for (int i = 0; i < temperatures.length; i++) {
+        for (int i = 0; i < length; i++) {
             while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
                 int index = stack.pop();
                 answer[index] = i - index;
