c29deeacbd54797f1498927d7b91065fbeb1eb19
Cori1304 <160833798+Cori1304-Seong@users.noreply.github.com>
2025-03-05 21:36:56 +0900

    Time: 1 ms (97.56%), Space: 45.6 MB (58.98%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int count = 0;
-
        
-        
         for (int i=0; i < flowerbed.length; i++){
             boolean leftEmpty = (i ==0 || flowerbed[i-1] ==0);
             boolean rightEmpty = (i==flowerbed.length -1 || flowerbed[i+1] ==0);
@@ -12,11 +10,8 @@ class Solution {
                 count++;
                 flowerbed[i] = 1;
             }
-
-
         }
 
         return n <= count ?true :false;
-
     }
 }
