5c3998f3bcf3d98b1f12644f96db7ecc169bc478
Cori1304 <160833798+Cori1304-Seong@users.noreply.github.com>
2025-10-21 23:42:49 +0900

    Time: 6 ms (85.15%), Space: 43.2 MB (57.89%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -2,16 +2,15 @@ import java.util.*;
 
 class Solution {
     public String reverseWords(String s) {
-        String[] strArr = s.split(" ");
+        s = s.trim();
+        String[] strArr = s.split("\\s+");
         StringBuilder answer = new StringBuilder();
+
         for (int i= strArr.length -1 ; i >= 0 ; i--) {
             String str = strArr[i];
 
-            if (str.contains(" ")) continue;
-
-            if (i < strArr.length -1 && strArr[i].matches("[a-zA-Z0-9]+")  ) answer.append(" ");
-
             answer.append(str);
+              if (i > 0) answer.append(" ");
 
 
         }
