611585b5b63736c5ef89a1ada205e7d31738a3bb
Cori1304 <160833798+Cori1304-Seong@users.noreply.github.com>
2025-11-15 14:25:49 +0900

    Time: 10 ms (79.05%), Space: 47.2 MB (14.64%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.java
@@ -1,71 +1,35 @@
-/**
-1. nums1, nums2 원소를 HashSet에 집어 넣기
-2. set1, set2가 서로 중복되는 원소가 있는지 체크
-    - 중복 되는 원소가 있다면 set1, set2 모두 원소 삭제
-3. 
- */
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-    List<List<Integer>> answer = new ArrayList();
-    Set<Integer> set1 = new HashSet();
-    Set<Integer> set2 = new HashSet();   
-    // List<Integer> temp = new ArrayList(); // 중복되는 원소 저장 
-    // answer.add(new ArrayList());
-    // answer.add(new ArrayList());
-    List<Integer> list1 = new ArrayList();
-    List<Integer> list2 = new ArrayList();
 
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
 
+        // 배열 → Set (중복 제거)
+        for (int n : nums1) set1.add(n);
+        for (int n : nums2) set2.add(n);
 
+        // nums1에만 있는 값
+        List<Integer> only1 = new ArrayList<>();
+        for (int n : set1) {
+            if (!set2.contains(n)) {
+                only1.add(n);
+            }
+        }
 
+        // nums2에만 있는 값
+        List<Integer> only2 = new ArrayList<>();
+        for (int n : set2) {
+            if (!set1.contains(n)) {
+                only2.add(n);
+            }
+        }
 
-    for (int n: nums1) {
-        set1.add(n);
-    }
+        List<List<Integer>> answer = new ArrayList<>();
+        answer.add(only1);
+        answer.add(only2);
 
-    for (int n: nums2) {
-        set2.add(n);
-    }
-
-
-    // for (int n: set1){
-    //     if (set2.contains(n)){
-    //         temp.add(n);
-    //     }
-    // }
-
-    // for (int n:temp) {
-    //     set1.remove(n);
-    //     set2.remove(n);
-    // }
-
-    // 앞에 for문 2개를 없에 후 결과
-    //     answer.get(0).add(n);
-    // }
-
-    // for(int n: set2){
-    //     answer.get(1).add(n);
-    // }
-
-
-    // num1 답 추가 
-    for(int n: set1){
-        if (!set2.contains(n)) list1.add(n);
-    }
-
-    // num2 답 추가 
-    for(int n: set2){
-        if (!set1.contains(n)) list2.add(n);
-    }
-
-
-    list1.sort((a,b) -> a.compareTo(b));
-    list2.sort((a,b) -> a.compareTo(b));
-
-    answer.add(list1);
-    answer.add(list2);
-
-
-    return answer;
+        return answer;
     }
 }
