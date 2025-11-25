7f2519037f6f59fbb1ccb4c04a58dd26b6dc4bb1
Cori1304 <160833798+Cori1304-Seong@users.noreply.github.com>
2025-11-25 11:01:21 +0900

    Time: 0 ms (100%), Space: 42.7 MB (19.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,26 @@
+import java.lang.*;
 
-//  DP[i] = max(DP[i-1], DP[i-2] + nums[i])
+// final DP(n) = Max(nums[n] + DP(n-2), DP(n-1))
+// 이유 작은 문제로 나누고 합치면 큰 문제를 해결할 수 있기 때문에
 
 class Solution {
     public int rob(int[] nums) {
         int len = nums.length;
-        int[] answer = new int[nums.length];
-        if (len < 2){
+        int[] answer = new int[len];
+
+        if (len <= 1) {
             return nums[0];
         }
 
         answer[0] = nums[0];
         answer[1] = Math.max(nums[0], nums[1]);
 
-        for (int i=2 ; i< len ; i++) {
+        for (int i = 2; i < len ; i++ ) {
             answer[i] = Math.max(answer[i-1], answer[i-2] + nums[i]);
-
         }
 
-        return answer[len-1];
+            // System.out.println(Arrays.toString(answer));
+
+            return Math.max(answer[len-1], answer[len-2]) ;
     }
 }
